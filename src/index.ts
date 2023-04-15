c18cd1d4ac988f08d08f0670265754301ad273fd
Diego Teixeira <diegosmteixeira@gmail.com>
2023-04-15 20:02:50 -0300

    First operators with rxjs: filter, map

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Observable, from } from "rxjs";
+import { Observable, filter, from, map } from "rxjs";
 
 let numbers = [1, 5, 10, 15, 20, 25, 30];
 
@@ -27,15 +27,21 @@ let source = new Observable(subscriber => {
 });
 
 
-// Observable(subscriber) will be executed when .subscribe
-source.subscribe({
-  next: (x: number) => console.log(x),
-  error: (e: Error) => console.log(e),
-  complete: () => console.log('Complete')
-});
 
-source.subscribe({
-  next: (x: number) => console.log(x),
-  error: (e: Error) => console.log(e),
-  complete: () => console.log('Complete')
-});
+
+// Observable(subscriber) will be executed when .subscribe
+source.pipe(
+  map((n: number) => n * 2)
+  ).subscribe({
+    next: (x: number) => console.log(x),
+    error: (e: Error) => console.log(e),
+    complete: () => console.log('Complete')
+  });
+  
+  source.pipe(
+    filter((n: number) => n > 4)
+  ).subscribe({
+    next: (x: number) => console.log(`filter: ${x}`),
+    error: (e: Error) => console.log(e),
+    complete: () => console.log('Complete')
+  });
